ec07937066d46234f2e4cf9dec612f818ce0f6b6
clc3123 <clc3123@gmail.com>
2013-09-22 12:55:58 +0800

    allow video type chosing

--- downloader.rb
+++ downloader.rb
@@ -4,49 +4,34 @@ require 'bundler/setup'
 require 'uri'
 require 'fileutils'
 require 'nokogiri'
-require 'open-uri'
 require 'json'
 require 'pry'
 
+FileUtils.mkdir_p 'tmp'
+
+PAGE_URL = ARGV[0] || 'http://www.youtube.com/watch?v=Gq76bcxM_gI'
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36"
-FileUtils.mkdir_p 'test'
 
-def import_youtube_page
-  # content = ''
-  # File.open('test/youtube.html', 'w') do |f|
-  #   puts 'Downloading youtube page...'
-  #   page_url = ARGV[0] || 'http://www.youtube.com/watch?v=Gq76bcxM_gI'
-  #   content = open(page_url).read
-  #   puts 'Downloaded!'
-  #   f.write content
-  #   puts 'Written to youtube.html!'
-  # end
-  # content
-
-  puts 'Downloading youtube page...'
-  page_url = ARGV[0] || 'http://www.youtube.com/watch?v=Gq76bcxM_gI'
-  system("curl", "-A", USER_AGENT, "-D", "test/header.txt", "-c", "test/cookie.txt", "-o", "test/youtube.html", page_url)
-  puts 'Written to youtube.html!'
-  File.read('test/youtube.html')
+def fetch_youtube_page
+  puts "Downloading youtube page #{PAGE_URL} ..."
+  system("curl", "-A", USER_AGENT, "-c", "tmp/cookie.txt", "-o", "tmp/youtube.html", PAGE_URL)
+  puts 'Page written to tmp/youtube.html!'
+  File.read('tmp/youtube.html')
 end
 
-html = Nokogiri::HTML(import_youtube_page)
-str = html.at_css('#player-api').next_element.next_element.content[48..-2]
-begin
-  ytconfig = JSON.parse(str)
-  File.open('test/ytconfig.json', 'w') do |f|
-    f.puts JSON.pretty_generate(ytconfig)
-  end
-rescue JSON::ParserError
-  raise "invalid json"
+puts "Analyzing youtube page #{PAGE_URL} ..."
+html = Nokogiri::HTML(fetch_youtube_page)
+ytconfig = JSON.parse(html.at_css('#player-api').next_element.next_element.content[48..-2])
+File.open('tmp/ytconfig.json', 'w') do |f|
+  f.puts JSON.pretty_generate(ytconfig)
 end
 
-File.open('test/fmt_list.json', 'w') do |f|
-  arr = []
-  ytconfig['args']['fmt_list'].split(',').each do |line|
-    arr << line
-  end
-  f.puts JSON.pretty_generate(arr)
+fmt_list = []
+ytconfig['args']['fmt_list'].split(',').each do |line|
+  fmt_list << line
+end
+File.open('tmp/fmt_list.json', 'w') do |f|
+  f.puts JSON.pretty_generate(fmt_list)
 end
 
 normal_fmts = []
@@ -59,7 +44,7 @@ ytconfig['args']['url_encoded_fmt_stream_map'].split(',').each do |i|
   h['download_url'] = h['url'] + '&' + URI.encode_www_form([['signature', signature]])
   normal_fmts << h
 end
-File.open('test/normal_fmts.json', 'w') do |f|
+File.open('tmp/normal_fmts.json', 'w') do |f|
   f.puts JSON.pretty_generate(normal_fmts)
 end
 
@@ -72,11 +57,39 @@ ytconfig['args']['adaptive_fmts'].split(',').each do |i|
   h['url_query_key_size'] = url_query.keys.size
   adaptive_fmts << h
 end
-File.open('test/adaptive_fmts.json', 'w') do |f|
+File.open('tmp/adaptive_fmts.json', 'w') do |f|
   f.puts JSON.pretty_generate(adaptive_fmts)
 end
 
-target = normal_fmts[1]['download_url']
-puts target
-system("curl", "-A", USER_AGENT, "-b", "test/cookie.txt", "-o", "test/youtube1.mp4", target)
-system("wget", "-Ncq", "-e", "convert-links=off", "--load-cookies", "/dev/null", "--tries=200", "--timeout=20", "--no-check-certificate", "-O", "test/youtube2.mp4", target);
+d_index = nil
+loop do
+  puts "Which format do you want to download?"
+  normal_fmts.each_with_index do |fmt, i|
+    puts "#{i.to_s.ljust(2)}, #{fmt['quality'].to_s.ljust(10)}, #{fmt['type'].to_s.ljust(50)}"
+  end
+  puts "Please type in the index:"
+  d_index = STDIN.gets.chomp.to_i
+  if normal_fmts.size > d_index and d_index >= 0
+    break
+  else
+    puts "Invalid index!"
+  end
+end
+
+target = normal_fmts[d_index]['download_url']
+target_extension = case normal_fmts[d_index]['type']
+                   when /webm/i
+                     'webm'
+                   when /mp4/i
+                     'mp4'
+                   when /flv/i
+                     'flv'
+                   when /3gpp/i
+                     '3gp'
+                   else
+                     raise 'unknown video type!'
+                   end
+puts "Start downloading using curl!"
+system("curl", "-L", "-A", USER_AGENT, "-b", "tmp/cookie.txt", "-o", "tmp/youtube1.#{target_extension}", target)
+puts "Start downloading using wget!"
+system("wget", "-Ncq", "-e", "convert-links=off", "--load-cookies", "/dev/null", "--tries=200", "--timeout=20", "--no-check-certificate", "-O", "tmp/youtube2.#{target_extension}", target);
